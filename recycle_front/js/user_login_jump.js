45dbc471144df2f3c5d3c14f999c428f52dad5d2
枫生 <zhoukuansky@163.com>
2019-03-03 17:48:11 +0800

    ..

---
+++ recycle_front/js/user_login_jump.js
@@ -0,0 +1,41 @@
+var token=getCookie("token");
+
+var personVue = new Vue({
+    el: '#information',
+    data: {
+        id:"",
+        tel:"",
+        address:"",
+        email:"",
+        name:"",
+    }
+})
+
+//alert(token)
+$.ajax({
+    url: url + "/user/userInformation",
+    type: "GET",
+    dataType: "json",
+    data: {
+    },
+    headers: {
+        token:token
+    },
+    success: function (res) {
+        if (res.status == 0) {
+            //alert(getCookie("token"))
+            personVue.id=res.data.id;
+            personVue.address=res.data.address;
+            personVue.tel=res.data.tel;
+            personVue.email=res.data.email;
+            personVue.name=res.data.name;
+        }
+        if(res.status==402) {
+            alert("token过期，请重新登录");
+            window.location.href = "login.html";
+        }
+    },
+    error: function (res) {
+        console.log(res);
+    }
+})
