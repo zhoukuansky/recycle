440b0116008336737d793d1c0db8b7b21c5a395d
枫生 <zhoukuansky@163.com>
2019-03-05 16:55:13 +0800

    0

--- recycle_front/ourJS/faq.js
+++ recycle_front/ourJS/faq.js
@@ -1,9 +1,12 @@
-var token=getCookie("token");
+var token = getCookie("token");
 
 var personVue = new Vue({
     el: '#rubbishType',
     data: {
-      items:[{id:"",rubbis_type:""}],
+        selected: "1",
+        kg: "",
+        info: "",
+        items: [{ id: "", rubbis_type: "" }],
     }
 })
 
@@ -14,15 +17,15 @@ $.ajax({
     data: {
     },
     headers: {
-        token:token
+        token: token
     },
     success: function (res) {
         if (res.status == 0) {
             //alert(getCookie("token"))
-            personVue.items=res.data.rows;
+            personVue.items = res.data.rows;
             console.log(personVue.items)
         }
-        if(res.status==402) {
+        if (res.status == 402) {
             alert("token过期，请重新登录");
             window.location.href = "login.html";
         }
@@ -30,4 +33,44 @@ $.ajax({
     error: function (res) {
         console.log(res);
     }
+})
+
+$("#submit").click(function () {
+    console.log(personVue.selected)
+    if (personVue.kg == "") {
+        alert("重量不能为空");
+    } else {
+        $.ajax({
+            url: url + "/odrers/insertOrders",
+            type: "POST",
+            dataType: "json",
+            data: {
+                moneny:personVue.kg*personVue.items[personVue.selected].rub_price,
+                order_rub_infor:personVue.info,
+                rub_id:personVue.items[personVue.selected].id,
+            },
+            headers: {
+                token: token
+            },
+            success: function (res) {
+                if (res.status == 0) {
+                    //alert(getCookie("token"))
+                    //personVue.items = res.data.rows;
+                    console.log(personVue.items)
+                    alert("新建订单成功！");
+                    personVue.selected="1";
+                    personVue.info="";
+                    personVue.kg="";
+                }
+                if (res.status == 402) {
+                    alert("token过期，请重新登录");
+                    window.location.href = "login.html";
+                }
+            },
+            error: function (res) {
+                console.log(res);
+            }
+        })
+    }
+
 })
