c1c73765ce9ea381a93f85e1b6ae9ae049926ece
KAMI-SRJ <775769547@qq.com>
2019-03-07 20:46:11 +0800

    .

--- recycle_front/ourJS/plans.js
+++ recycle_front/ourJS/plans.js
@@ -1,10 +1,16 @@
 var token = getCookie('token')
 
 var ob = new Vue({
-	el: '#list',
+	el: '#content',
 	data: {
-		orderList: [],
+		orderList1: [],
+		orderList2: [],
 		rubList: []
+	},
+	methods: {
+		getRubType : function () {
+			return ;
+		}
 	}
 })
 
@@ -17,24 +23,42 @@ $.ajax({
 	},
 	data: {
 		pageNum: 1,
-		pageSize: 3
+		pageSize: 3,
+		status: 1  /*1.創建  2.處理 3.處理完成未評分 4.評分完成*/
 	},
+	success: function(res){
+		ob.orderList1 = res.data.rows
+	}
+})
+
+
+$.ajax({
+	type: "GET",
+	url: url + "/odrers/userFindOrdersList",
+	async: true,
+	headers: {
+		token: token
+	},
+	data: {
+		pageNum: 1,
+		pageSize: 3,
+		status: 4  /*1.創建  2.處理 3.處理完成未評分 4.評分完成*/
+	},
+	success: function(res){
+		ob.orderList2 = res.data.rows
+	}
+})
+
+$.ajax({
+	type: "GET",
+	url: url + "/rubbish/rubbishList",
+	async: true,
+	headers: {
+		token: token
+	},
+	data: {},
 	success: function(res) {
-		ob.orderList = res.data.rows
-		console.log(ob.orderList)
-		$.ajax({
-			type: "GET",
-			url: url + "/rubbish/rubbishInformation",
-			async: true,
-			headers: {
-				token: token
-			},
-			data: {
-				id : 2,
-			},
-			success: function(res){
-				
-			}
-		});
+		ob.rubList = res.data.rows
 	}
 });
+
